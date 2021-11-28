75b4136dea06ae0fd58d167d9d0e78efafcc02e2
Shaurya <guptashaurya2002@gmail.com>
2021-11-29 00:04:15 +0530

    git introo

--- World.js
+++ World.js
@@ -1,1 +1,10 @@
 console.log("World is very small");
+
+console.log ('\"git init .\" for creating the new git repositiry')
+console.log(" \"git add <new file name> \" for adding the file to git")
+console.log ('\"git status\" for checking the status of git')
+console.log(" \"git commit -m \"<new file name>\" \" for commiting the file to git")
+console.log ('\"git log\" for checking the history of commits done')
+console.log ('\"git log --oneline\" for checking the history of commits done in one line')
+console.log ('\"git checkout -b <name of new branch>\" for checking the history of commits done')
+console.log ('\"git branch\" to know the status of current branch')
