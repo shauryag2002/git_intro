411ce37c53cc5cb1ddefe0f1b469c22cbb429559
Shaurya <guptashaurya2002@gmail.com>
2021-11-29 15:22:37 +0530

    git fulltree info to master

--- World.js
+++ World.js
@@ -10,3 +10,4 @@ console.log ('\"git log --oneline\" for checking the history of commits done in 
 console.log ('\"git checkout -b <name of new branch>\" for checking the history of commits done')
 console.log ('\"git branch\" to know the status of current branch')
 console.log("\"git checkout <name of branch to switch >\" to switch branch")
+console.log("\"git config --global alias.fulltree \"log --color --oneline --graph --decorate --all\"\"now only type \"git fulltree\" to see the log in tree format")
